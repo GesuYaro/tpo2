c8b25521975e0f4968f0f5a3f23ad3ff5c84c918
sshagiev <sshagiev@phoenixit.ru>
2023-05-03 20:02:59 +0300

    add Trigonometry

--- src/main/java/shagiev_dobryagin/Trigonometry.java
+++ src/main/java/shagiev_dobryagin/Trigonometry.java
@@ -2,7 +2,38 @@ package shagiev_dobryagin;
 
 public class Trigonometry {
 
-    public double cos(double x) {
+    private final TrigonometrySeries trigonometrySeries;
 
+    public Trigonometry(TrigonometrySeries trigonometrySeries) {
+        this.trigonometrySeries = trigonometrySeries;
+    }
+
+    public double cos(double x, double e) {
+        x = Math.abs(x);
+        x %= (2 * Math.PI);
+        if (x > Math.PI) x-= 2*Math.PI;
+        x = Math.abs(x);
+        return trigonometrySeries.cos(x, e);
+    }
+
+    public double sin(double x, double e) {
+        x = Math.PI/2 - x;
+        return cos(x, e);
+    }
+
+    public double sec(double x, double e) {
+        return 1 / sin(x, e);
+    }
+
+    public double csc(double x, double e) {
+        return 1 / cos(x, e);
+    }
+
+    public double tan(double x, double e) {
+        return sin(x, e) / cos(x, e);
+    }
+
+    public double ctg(double x, double e) {
+        return cos(x, e) / sin(x, e);
     }
 }
--- src/main/java/shagiev_dobryagin/TrigonometrySeries.java
+++ src/main/java/shagiev_dobryagin/TrigonometrySeries.java
@@ -3,12 +3,16 @@ package shagiev_dobryagin;
 public class TrigonometrySeries {
 
     public double cos(double x, double e) {
+        double elem;
         double sum = 1;
         long fakt2n = 2;
-        int n = 1;
+        long n = 1;
         do {
-            fakt2n = fakt2n * (fakt2n + 1) * (fakt2n + 2);
-            double elem = Math.pow(-1, n) * Math.pow(x, 2*n) / fakt2n;
-        }
+            elem = Math.pow(-1, n) * Math.pow(x, 2*n) / fakt2n;
+            fakt2n = fakt2n * (2*n + 1) * (2*n + 2);
+            sum += elem;
+            n++;
+        } while (Math.abs(elem) > e);
+        return sum;
     }
 }
