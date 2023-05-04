87ee8d5029cb7823bd21ab9854290a0c528a90f2
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2023-05-04 13:59:46 +0300

    Add Logarifmometry

---
+++ src/main/java/shagiev_dobryagin/Logarifmometry.java
@@ -0,0 +1,32 @@
+package shagiev_dobryagin;
+
+import static java.lang.Math.pow;
+
+public class Logarifmometry {
+  private static final double EPS = 0.001;
+
+  public double ln(double t) {
+    return ln(t, EPS);
+  }
+
+  public double ln(double t, double eps) {
+    if (t <= 0.) {
+      throw new IllegalArgumentException();
+    }
+
+    final double yFirst = 20;
+    double yCur;
+    double yNext = yFirst;
+
+    do {
+      yCur = yNext;
+      yNext = yCur - 1 + t / pow(Math.E, yCur);
+    } while (yCur - yNext > eps);
+
+    return yNext;
+  }
+
+  public double log(double base, double x) {
+    return ln(x) / ln(base);
+  }
+}
