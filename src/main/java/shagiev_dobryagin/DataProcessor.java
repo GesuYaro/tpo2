85f450b9ca759e4137fb344c7e517af477024692
sshagiev <sshagiev@phoenixit.ru>
2023-05-05 01:06:58 +0300

    change trigonometry

---
+++ src/main/java/shagiev_dobryagin/DataProcessor.java
@@ -0,0 +1,16 @@
+package shagiev_dobryagin;
+
+import java.io.File;
+import java.util.List;
+import java.util.function.Function;
+
+public class DataProcessor {
+
+
+    public void writeResultsToFile(List<Double> x, Function<Double, Double> func, File file) {
+        var y = x.stream()
+                .map(d -> List.of(d.toString(), func.apply(d).toString()))
+                .toList();
+        CsvReader.writeCsv(file, y);
+    }
+}
